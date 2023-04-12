3f8d7fbef185d404a2d9391681be4b532605a235
Rafael Azevedo <rafael.nqda@gmail.com>
2023-04-12 15:25:47 +0100

    Update tests

--- packages/stylelint-config-react/test/index.test.js
+++ packages/stylelint-config-react/test/index.test.js
@@ -4,7 +4,6 @@
  */
 
 import config from '../src';
-import fs from 'fs';
 import stylelint from 'stylelint';
 
 /**
@@ -14,8 +13,8 @@ import stylelint from 'stylelint';
 describe('@untile/stylelint-config', () => {
   it('correct', async () => {
     const result = await stylelint.lint({
-      code: fs.readFileSync('./test/fixtures/correct.js', 'utf-8'), // eslint-disable-line no-sync
-      config
+      config,
+      files: './test/fixtures/correct.js'
     });
 
     expect(result.errored).toBeFalsy();
@@ -24,8 +23,8 @@ describe('@untile/stylelint-config', () => {
 
   it('incorrect', async () => {
     const result = await stylelint.lint({
-      code: fs.readFileSync('./test/fixtures/incorrect.js', 'utf-8'), // eslint-disable-line no-sync
-      config
+      config,
+      files: './test/fixtures/incorrect.js'
     });
 
     const warnings = JSON.parse(result?.output)[0].warnings;
@@ -35,37 +34,14 @@ describe('@untile/stylelint-config', () => {
       'order/properties-order',
       'alpha-value-notation',
       'alpha-value-notation',
-      'color-hex-case',
       'color-hex-length',
-      'declaration-bang-space-after',
-      'declaration-bang-space-before',
-      'declaration-block-trailing-semicolon',
-      'declaration-colon-space-after',
       'font-family-name-quotes',
-      'function-comma-space-after',
-      'function-comma-space-before',
       'function-name-case',
-      'function-parentheses-newline-inside',
-      'function-parentheses-newline-inside',
-      'function-parentheses-space-inside',
-      'function-parentheses-space-inside',
-      'function-parentheses-space-inside',
-      'function-parentheses-space-inside',
       'function-url-quotes',
-      'function-whitespace-after',
       'length-zero-no-unit',
-      'no-extra-semicolons',
-      'number-leading-zero',
-      'number-no-trailing-zeros',
-      'property-case',
-      'selector-pseudo-class-case',
-      'selector-pseudo-element-case',
       'selector-pseudo-element-colon-notation',
       'shorthand-property-no-redundant-values',
-      'string-quotes',
-      'unit-case',
-      'value-keyword-case',
-      'indentation'
+      'value-keyword-case'
     ]);
   });
 });
