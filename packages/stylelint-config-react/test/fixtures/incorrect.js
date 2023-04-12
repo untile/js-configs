68c2dad7528a05b797d7a38041276563f6dd478a
Rafael Azevedo <rafael.nqda@gmail.com>
2023-04-12 15:25:47 +0100

    Update tests

--- packages/stylelint-config-react/test/fixtures/incorrect.js
+++ packages/stylelint-config-react/test/fixtures/incorrect.js
@@ -10,36 +10,9 @@ const AlphaValueNotation = styled.div`
   opacity: 50%;
 `;
 
-// `color-hex-case`.
-const ColorHexCase = styled.div`
-  color: #FFFFFF;
-`;
-
 // `color-hex-length`.
 const ColorHexLength = styled.div`
   color: #fff;
-`;
-
-// `declaration-bang-space-after`.
-// `declaration-bang-space-before`.
-const DeclarationBangSpace = styled.div`
-  color: #ffffff ! important;
-  display: none!important;
-`;
-
-// `declaration-block-semicolon-newline-after`.
-// `declaration-block-semicolon-space-after`.
-// `declaration-block-semicolon-space-before`.
-// `declaration-block-trailing-semicolon`.
-const DeclarationBlockSemicolon = styled.div`
-  color: #ffffff; display: block;
-  position: relative ;
-  top: 0
-`;
-
-// `declaration-colon-space-after`.
-const DeclarationColonSpaceAfter = styled.div`
-  display:block;
 `;
 
 // `font-family-name-quotes`.
@@ -47,31 +20,9 @@ const FontFamilyNameQuotes = styled.div`
   font-family: Times New Roman, Times, serif;
 `;
 
-// `function-comma-space-after`.
-const FunctionCommaSpaceAfter = styled.div`
-  transform: translate(1,1);
-`;
-
-// `function-comma-space-before`.
-const FunctionCommaSpaceBefore = styled.div`
-  transform: translate(1 , 1);
-`;
-
 // `function-name-case`.
 const FunctionNameCase = styled.div`
   width: Calc(5% - 1em);
-`;
-
-// `function-parentheses-newline-inside`.
-const FunctionParenthesesNewlineInside = styled.div`
-  transform: translate(
-    1, 1
-  );
-`;
-
-// `function-parentheses-space-inside`.
-const FunctionParenthesesSpaceInside = styled.div`
-  transform: translate( 1, 1 );
 `;
 
 // `function-url-quotes`.
@@ -79,48 +30,9 @@ const FunctionUrlQuotes = styled.div`
   background: url(x.jpg);
 `;
 
-// `function-whitespace-after`.
-const FunctionWhitespaceAfter = styled.div`
-  transform: translate(1, 1)scale(3);
-`;
-
 // `length-zero-no-unit`.
 const LengthZeroNoUnit = styled.div`
   top: 0px;
-`;
-
-// `no-extra-semicolons`.
-const NoExtraSemicolons = styled.div`
-  display: block;;
-`;
-
-// `number-leading-zero`.
-const NumberLeadingZero = styled.div`
-  line-height: .5;
-`;
-
-// `number-no-trailing-zeros`.
-const NumberNoTrailingZeros = styled.div`
-  top: 1.0px;
-`;
-
-// `property-case`.
-const PropertyCase = styled.div`
-  WIDTH: 1px;
-`;
-
-// `selector-pseudo-class-case`.
-const selectorPseudoClassCase = styled.div`
-  :Hover {
-    color: #000000;
-  }
-`;
-
-// `selector-pseudo-element-case`.
-const SelectorPseudoElementCase = styled.div`
-  &::Before {
-    content: '';
-  }
 `;
 
 // `selector-pseudo-element-colon-notation`.
@@ -135,22 +47,7 @@ const ShorthandPropertyNoRedundantValues = styled.div`
   margin: 1px 1px;
 `;
 
-// `string-quotes`.
-const StringQuotes = styled.div`
-  content: "";
-`;
-
-// `unit-case`.
-const UnitCase = styled.div`
-  top: 1Px;
-`;
-
 // `value-keyword-case`.
 const ValueKeywordCase = styled.div`
   display: BLOCK;
-`;
-
-// `indentation`.
-const Indentation = styled.div`
- display: block;
 `;
