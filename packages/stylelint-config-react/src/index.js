78723ec129682b2e85c9ee058e91a0b14c898d76
Rafael Azevedo <rafael.nqda@gmail.com>
2023-04-12 15:24:57 +0100

    Update custom syntax processor and update rules
    This commit removes deprecated rules.

--- packages/stylelint-config-react/src/index.js
+++ packages/stylelint-config-react/src/index.js
@@ -14,7 +14,7 @@ const w3cList = [...new Set(cssProperties.map(({ property }) => property))];
  */
 
 module.exports = {
-  customSyntax: '@stylelint/postcss-css-in-js',
+  customSyntax: 'postcss-styled-syntax',
   extends: [
     'stylelint-config-recommended',
     'stylelint-config-styled-components'
@@ -22,32 +22,12 @@ module.exports = {
   plugins: ['stylelint-order'],
   rules: {
     'alpha-value-notation': 'number',
-    'color-hex-case': 'lower',
     'color-hex-length': 'long',
-    'declaration-bang-space-after': 'never',
-    'declaration-bang-space-before': 'always',
-    'declaration-block-semicolon-newline-after': 'always',
-    'declaration-block-semicolon-space-after': 'always',
-    'declaration-block-semicolon-space-before': 'never',
-    'declaration-block-trailing-semicolon': 'always',
-    'declaration-colon-space-after': 'always-single-line',
     'font-family-name-quotes': 'always-where-recommended',
-    'function-comma-space-after': 'always',
-    'function-comma-space-before': 'never',
     'function-name-case': 'lower',
-    'function-parentheses-newline-inside': 'never-multi-line',
-    'function-parentheses-space-inside': 'never',
     'function-url-quotes': 'always',
-    'function-whitespace-after': 'always',
-    indentation: 2,
     'length-zero-no-unit': true,
-    'max-empty-lines': 1,
-    'no-eol-whitespace': true,
-    'no-extra-semicolons': true,
-    'number-leading-zero': 'always',
-    'number-no-trailing-zeros': true,
     'order/properties-order': w3cList,
-    'property-case': 'lower',
     'rule-empty-line-before': [
       'always-multi-line', {
         except: 'first-nested',
@@ -55,9 +35,6 @@ module.exports = {
       }
     ],
     'selector-attribute-quotes': 'always',
-    'selector-combinator-space-after': 'always',
-    'selector-pseudo-class-case': 'lower',
-    'selector-pseudo-element-case': 'lower',
     'selector-pseudo-element-colon-notation': 'double',
     'selector-type-case': 'lower',
     'selector-type-no-unknown': [true, {
@@ -65,8 +42,6 @@ module.exports = {
       ignoreTypes: ['/-styled-mixin/', '$dummyValue']
     }],
     'shorthand-property-no-redundant-values': true,
-    'string-quotes': 'single',
-    'unit-case': 'lower',
     'value-keyword-case': ['lower', {
       ignoreKeywords: ['dummyValue']
     }]
