7f07a6d2966e2e4497c8daeca1e977cca3ff5bbf
rafaelcruzazevedo <el.rafadcazevedo@gmail.com>
2023-06-05 12:14:02 +0100

    Update eslint-config-typescript-react package
    This commit extends from @untile/eslint-config-typescript package.

--- packages/eslint-config-typescript-react/src/index.js
+++ packages/eslint-config-typescript-react/src/index.js
@@ -5,27 +5,12 @@
 module.exports = {
   extends: [
     '@untile/eslint-config-react',
-    'plugin:@typescript-eslint/recommended'
+    '@untile/eslint-config-typescript'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true
     }
-  },
-  plugins: ['@typescript-eslint', 'typescript-sort-keys'],
-  rules: {
-    '@typescript-eslint/comma-dangle': ['error'],
-    '@typescript-eslint/explicit-member-accessibility': 0,
-    '@typescript-eslint/explicit-module-boundary-types': 0,
-    '@typescript-eslint/indent': 0,
-    '@typescript-eslint/no-explicit-any': 0,
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/no-use-before-define': ['error'],
-    'comma-dangle': 0,
-    'no-unused-vars': 0,
-    'no-use-before-define': 0,
-    'typescript-sort-keys/interface': 'error'
   }
 };
-
