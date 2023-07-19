ff54b3ef23341bfcc22d8f80bb228f0a6a5a5435
rafaelcruzazevedo <el.rafadcazevedo@gmail.com>
2023-07-19 16:38:02 +0100

    Bump prettier to version 3

--- packages/prettier-config/test/index.test.js
+++ packages/prettier-config/test/index.test.js
@@ -11,31 +11,28 @@ const prettier = require('prettier');
 const prettierConfig = require('../src/index');
 
 /**
- * Check.
- */
-
-const check = (content) => prettier.check(content, {
-  ...prettierConfig,
-  parser: 'babel'
-});
-
-/**
  * Tests for `@untile/prettier-config`.
  */
 
 describe('@untile/prettier-config', () => {
-  it('should not generate any violation for correct code', () => {
+  it('should not generate any violation for correct code', async () => {
     const source = path.resolve(__dirname, 'fixtures', 'correct.js');
     const content = fs.readFileSync(source, 'utf8');
-    const results = check(content);
+    const results = await prettier.check(content, {
+      ...prettierConfig,
+      parser: 'babel'
+    });
 
     expect(results).toBe(true);
   });
 
-  it('should generate violations for incorrect code', () => {
+  it('should generate violations for incorrect code', async () => {
     const source = path.resolve(__dirname, 'fixtures', 'incorrect.js');
     const content = fs.readFileSync(source, 'utf8');
-    const results = check(content);
+    const results = await prettier.check(content, {
+      ...prettierConfig,
+      parser: 'babel'
+    });
     
     expect(results).toBe(false);
   });
